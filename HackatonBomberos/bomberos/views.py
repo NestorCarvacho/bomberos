1ea80c3012feebdcf381e54c410a7bee973de610
Nicolás Muñoz <101838213+Xeroblic@users.noreply.github.com>
2023-06-10 03:16:13 -0400

    funciones basicas

--- HackatonBomberos/bomberos/views.py
+++ HackatonBomberos/bomberos/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def lista_cuarteles(request):
+    cuarteles = Cuartel.objects.all();
+
+    for cuartel in cuarteles:
+        cuartel.comuna_nombre = cuartel.comuna.nombre
+    
+    return render(request, 'lista_cuarteles.html', {'cuarteles' : cuarteles})
+
+def form_mod_cuartel(request, id):
+    cuartel = Cuartel.objects.filter(idCuartel=id).first()
+
+    datos = {
+        'form': CuartelForm(instance=cuartel)
+    }
+
+    if request.method == 'POST':
+        formulario = CuartelForm(data=request.POST, instance=cuartel)
+        if formulario.is_valid():
+            formulario.save()
+
+    return render(request, 'form_mod_cuartel.html', datos)
+
+def form_del_cuartel(request, id):
+    cuarteles = Cuartel.objects.filter(idCuartel=id)
+    
+    for cuartel in cuarteles:
+        cuartel.delete()
+    
+    return redirect(to="lista_cuarteles")
